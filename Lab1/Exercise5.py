983d3ea6f9231f23c44348d6957eb45a4f18aa05
Asif Rasheed <49068949+asifrasheed6@users.noreply.github.com>
2020-02-11 17:36:01 +0400

    Add files via upload

---
+++ Lab1/Exercise5.py
@@ -0,0 +1,11 @@
+#Authors Abdullah Khan, Asif Rasheed
+#Programming Languages (CMP 321) Lab 1 Exercise 5
+
+s = 'Welcome to Python'
+print('The String: ', s)
+print('All characters between index 6 and 10: ',s[6:10])
+print('All Characters after the 4th character: ',s[4:])
+print('Last 5 characters: ',s[-5:])
+print('All characters at Even index: ',s[::2])
+print('The String in Reverse: ',s[::-1])
+print('Is it a palindrome?  ',s[::-1]==s)
