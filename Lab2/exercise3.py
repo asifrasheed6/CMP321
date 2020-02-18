f8f7503a567d3b4d5d0b08344636d7fdb557e860
Asif Rasheed <asif@linuxmail.org>
2020-02-18 19:01:07 +0400

    Add files via upload

---
+++ Lab2/exercise3.py
@@ -0,0 +1,28 @@
+#Authors Abdullah Khan, Asif Rasheed
+#Programming Languages (CMP 321) Lab 2 Exercise 3
+
+file =  open('email.txt', 'r', encoding ='unicode_escape').readlines()
+
+for line in file:
+    line = line.replace('\n','') #removes new line character
+    words = line.split(' ') #splits the line into words
+    
+    for word in words:
+        if '@'in word:
+            #If the word is an email address, searches for domains
+            valid = False
+            
+            domains = ['com', 'edu', 'gov', 'org']
+            word = word.replace(';','')
+            elements = word.split('@')
+
+            for element in elements:
+                if '.' in element:
+                    key = element.split('.')
+                    
+                    for domain in domains:
+                        if key[-1] in domain:
+                            valid = True
+
+            print(word, 'is '+(not valid)*'in'+'valid')
+
