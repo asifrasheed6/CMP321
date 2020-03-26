c941696477e74475902112fd3fe38f1107fca863
Asif Rasheed <asif@linuxmail.org>
2020-03-26 09:38:16 +0400

    Update exercise3.py

--- Lab7/exercise3.py
+++ Lab7/exercise3.py
@@ -3,11 +3,7 @@
 
 # PART A
 # Checks if the given number is prime or not
-def isPrime(num):
-    for i in range(2,int(num**(1/2))+1):
-        if num%i == 0 :
-            return False
-    return True
+def isPrime(n, i=2): return True if i>(n**(1/2)) else False if n%i==0 else isPrime(n,i+1)
 
 # Returns prime number larger than the given argument
 def genPrime(i=1):
