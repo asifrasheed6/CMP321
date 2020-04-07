9b4c6265c547704178062d38bc1a7f3721811d3c
Asif Rasheed <b00073877@aus.edu>
2020-04-07 18:00:40 +0400

    Adding Lab 8 Exercise 1 Part A

--- Lab8/exercise2.py
+++ Lab8/exercise2.py
@@ -5,6 +5,7 @@ import urllib.request
 
 import urllib;from urllib.request import urlopen
 
+# Part a
 url="https://sketchengine.co.uk/wp-content/uploads/word-list/english/english-word-list-total.csv"
 
 lst = []
@@ -15,4 +16,11 @@ try:
 except urllib.error.URLError:
     print("Error: Check the url")
 
-print(lst)
+for i in range(4): list.pop(i) # Ignoring the first 4 lines
+
+lst_words = [] # List of words
+for lines in list:
+    words = lines.split(';')
+    lst_words.append(words[1])
+
+print(lst_words)
