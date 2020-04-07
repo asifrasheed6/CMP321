c76bab786cc644adda55dff46be5e10f0a4e3012
Asif Rasheed <b00073877@aus.edu>
2020-04-07 17:21:48 +0400

    Lab 8 exercise1

--- Lab8/exercise1.py
+++ Lab8/exercise1.py
@@ -1,5 +1,41 @@
 #CMP 321 Lab 8 Exercise 1
 #Authors Abdullah Khan, Asif Rasheed
+import random
+import string
 
+# Part a
 def RandomCipher():
-    
+    ret = {}
+    for key, val in zip(string.ascii_lowercase, random.sample(string.ascii_lowercase,26)):
+        ret[key]=val
+    return ret
+
+dic = RandomCipher();
+
+# Part b
+def Encode(text):
+    ret = []
+    for i in range(len(text)):
+        if text[i].lower() in string.ascii_lowercase:
+            ret.append(dic[text[i].lower()])
+        else:
+            ret.append(text[i])
+    return ''.join(ret)
+
+# Part c
+def Decode(text):
+    ret = []
+    dec = {v: k for k, v in dic.items()}
+    for i in range(len(text)):
+        if text[i] in string.ascii_lowercase:
+            ret.append(dec[text[i]])
+        else:
+            ret.append(text[i])
+    return ''.join(ret)
+            
+
+text = "hellow, world!"
+text = Encode(text);
+print("Encoded Text: ",text)
+text = Decode(text);
+print("Decoded Text: ",text)
