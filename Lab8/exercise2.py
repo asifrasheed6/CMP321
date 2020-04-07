7de8a3df534eaa2acd443e808b7a9d144c8b560b
Asif Rasheed <b00073877@aus.edu>
2020-04-07 17:52:35 +0400

    Adding Lab8 Exericise 2

--- Lab8/exercise2.py
+++ Lab8/exercise2.py
@@ -1,2 +1,16 @@
 #CMP 321 Lab 8 Exercise 2
 #Authors Abdullah Khan, Asif Rasheed
+import csv
+import urllib.request
+
+import urllib;from urllib.request import urlopen
+
+url="https://sketchengine.co.uk/wp-content/uploads/word-list/english/english-word-list-total.csv"
+
+lst = []
+
+try:
+    for line in urlopen(url):
+        lst.append(line.decode('utf8'))
+except urllib.error.URLError:
+    print("Error: Check the url")
