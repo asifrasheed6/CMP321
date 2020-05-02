fd9ff0743865e6f1b905c38046ef6d45ee2df88d
Asif Rasheed <b00073877@aus.edu>
2020-05-02 21:08:48 +0400

    Exercise 1

--- Lab11/exercise1.py
+++ Lab11/exercise1.py
@@ -1,3 +1,12 @@
 # Programming Languages Lab (CMP321L)
 # Lab 11 Exercise 1
 # Authors Asif Rasheed, Abdullah Khan
+import re
+
+while True:
+    number = input('Enter phone number: ')
+
+    if re.search('([(]\d\d\d[)]\s|\d\d\d[-]|[0])([234679]|50|55|56)-\d\d\d\d\d\d\d',number):
+        print('The number is valid!')
+    else:
+        print('Invalid Number!')
