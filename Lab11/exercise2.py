80de4521a2a455a2d2b0953bc8e995445c2afdc1
Asif Rasheed <b00073877@aus.edu>
2020-05-02 22:47:27 +0400

    exercise2

--- Lab11/exercise2.py
+++ Lab11/exercise2.py
@@ -1,4 +1,19 @@
 # Programming Languages Lab (CMP321L)
 # Lab 11 Exercise 2
 # Authors Asif Rasheed, Abdullah Khan
+import re
 
+op = '[\+\-\*\/\%]'
+letter = '([a-z]|[A-Z])'
+digit = '[0-9]'
+numerical_literal = '(\+|\-)'+digit+'+.'+digit+'+'
+identifier = letter+'('+letter+'|'+digit+')*'
+assignment_statement = '('+identifier+')\=(('+identifier+')|'+'('+numerical_literal + '(('+op+identifier+')|('+numerical_literal+'))*))'
+
+while True:
+    assignment = input('Enter Assignment: ')
+
+    if re.search(assignment_statement,assignment):
+        print('Valid syntax!')
+    else:
+        print('Invalid syntax!')
